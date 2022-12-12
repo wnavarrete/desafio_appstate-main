b736e2e86f4ba613da405c1412a5200f0ff5c4c2
walternavarrete <walter.navarrete@outlook.com>
2022-12-12 20:58:06 -0300

    add static data

---
+++ src/constants/data/orders.js
@@ -0,0 +1,12 @@
+export const CATEGORIES = [
+  {
+    id: 1,
+    title: 'Harinas Puras',
+    color: '#FFD151',
+  },
+  {
+    id: 2,
+    title: 'Harinas Integrales',
+    color: '#FFDDA1',
+  },
+]
--- src/screens/delivered/index.js
+++ src/screens/delivered/index.js
@@ -3,13 +3,33 @@ import { View, Text, Button } from 'react-native';
 import { Card } from "../../components";
 import COLORS from "../../constants/colors";
 import { styles } from './styles';
+import { OrderItem } from '../../components';
+import { CATEGORIES } from '../../constants/data/index';
+import { FlatList, SafeAreaView } from 'react-native';
+
 
 
 const Delivered = ({navigation}) => {
   
+    const onSelected = (item) => {
+        console.warn('onSelected', item);
+      };
+
+      const renderItem = ({ item }) => <OrderItem item={item} onSelected={onSelected} />;
+
     return (
         <View style={styles.container}>
-            <Card style={styles.content}>
+
+<SafeAreaView style={styles.container}>
+       <FlatList
+         data={CATEGORIES}
+         renderItem={renderItem}
+         keyExtractor={(item) => item.id.toString()}
+         style={styles.containerList}
+       />
+     </SafeAreaView>
+
+            {/* <Card style={styles.content}>
                 <Text style={styles.title} color={COLORS.primary}>Delivered Screen</Text>
                 <View style={styles.containerButton}>
                     <Button
@@ -18,7 +38,7 @@ const Delivered = ({navigation}) => {
                         color={COLORS.primary}
                     />
                 </View>
-            </Card>
+            </Card> */}
         </View>
     )
 }
